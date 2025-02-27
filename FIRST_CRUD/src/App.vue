41a6aa79f1b3fcbabaa1abf1963d361c67c3f355
Alan Colli <150622900+alan-colli@users.noreply.github.com>
2025-02-27 14:21:27 -0300

    att

---
+++ FIRST_CRUD/src/App.vue
@@ -0,0 +1,17 @@
+<script setup></script>
+
+<template>
+  <div
+    class="bg-black flex justify-center items-center min-h-screen w-[100vw] flex-col"
+  >
+    <div class="bg-gray-200 h-[70vh] flex flex-col rounded-md">
+      <div
+        class="bg-black mt-12 h-[15vh] mx-4 flex flex-col w-[80vw] items-center rounded-md"
+      >
+        <input type="text" class="bg-gray-200 w-[40vw] m-2 px-1 rounded-md" />
+        <input type="text" class="bg-gray-200 w-[40vw] m-2 px-1 rounded-md" />
+        <button class="bg-blue-600 rounded-md text-white p-2">SUBMIT</button>
+      </div>
+    </div>
+  </div>
+</template>
